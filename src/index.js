161eafddf932c31b7305600e76e107d3807b1f4e
pbxyz <pablexyz@hotmail.com>
2021-10-03 03:13:57 -0430

    react-fundamentals No.001

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = 'Gerardo Gallegos'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user1 = {
+    name: 'Gerardo Gallegos',
+    age: 28,
+    country: 'Mexico'
+}
+
+function getInfo(user) {
+    return `Hola mi nombre es ${user.name} y soy de ${user.country}`
+}
+
+const App = <h1>{getInfo(user1)}</h1>
+
+ReactDOM.render(App, document.getElementById('root'))
