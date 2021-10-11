c22cb5c61ba3fac75e42a98d1b2bf79a61122096
pbxyz <pablexyz@hotmail.com>
2021-10-11 12:15:40 -0430

    react-fundamentals No.008

--- src/App.js
+++ src/App.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-const Title = (props) => {
-  const styles = {
-    background: props.uiColor,
-    padding: '0.3em',
-    color: '#FFF',
-    borderRadius: '0.3em',
-    textAlign: 'center',
-    fontSize: '50px'
+class PortalModal extends Component {
+
+  render () {
+
+    if (!this.props.visible) {
+      return null
+    }
+
+    const styles = {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      background: 'linear-gradient(to top right, #667eea, #764ba2',
+      opacity: '0.9',
+      color: '#FFF'
+    }
+  
+    return ReactDOM.createPortal((
+      <div style={styles}>
+        { this.props.children }
+      </div>
+    ), document.getElementById('modal-root'))
   }
-  console.log(`hola`)
-  return (
-    <h1 style={styles}>
-      {props.children}
-    </h1>
-  )
 }
-
 
 class App extends Component {
   state = {
-    uiColor: 'purple'
+    visible: false,
+    num: 0
   }
 
-  render() {
+  componentDidMount () {
+    setInterval(() => {
+      this.setState(state => ({
+        ...state,
+        num: state.num + 1
+      }))
+    }, 1000)
+  }
+
+  mostrar = () => {
+    this.setState({ visible: true })
+  }
+
+  cerrar = () => {
+    this.setState({ visible: false })
+  }
+
+  render () {
     return (
       <div>
-        <Title
-          uiColor={this.state.uiColor}
-        >
-          Super <em>Ninja</em>
-        </Title>
+        <button onClick={this.mostrar}>
+          Mostrar
+        </button>
+        <PortalModal visible={this.state.visible}>
+          Hola desde un PortalModal 😲 { this.state.num }
+          <button onClick={this.cerrar}>
+            Cerrar
+          </button>
+        </PortalModal>
       </div>
     )
   }
